2c63ef3524ecd757d8f56fea14d50f11f4cd3682
kartikgulati <kartikgulati19@gmail.com>
2025-02-21 12:02:19 -0500

    dark mode in the clerk user profile page.

--- src/app/(main)/Navbar.tsx
+++ src/app/(main)/Navbar.tsx
@@ -1,14 +1,18 @@
-"use  Client";
+"use client";
 
 import Link from "next/link";
 import Image from "next/image";
 import { UserButton } from "@clerk/nextjs";
 import { CreditCard } from "lucide-react";
 import ThemeToggle from "@/components/ThemeToggle";
+import {dark} from "@clerk/themes";
+import {useTheme } from "next-themes"; 
 
 
 
 export default function Navbar() {
+    const {theme} = useTheme();
+
   return (
     <header className="shadow-sm">
       <div className="max-w-7xl mx-auto flex items-center justify-between gap-10 p-3">
@@ -28,11 +32,11 @@ export default function Navbar() {
         <div className="flex items-center gap-3">
         <ThemeToggle />
         <UserButton
+         
           appearance={{
+            baseTheme: theme === "dark" ? dark : undefined,
             elements: {
-              avatar: true,
-              greeting: false,
-              name: false,
+              
               signOut: true,
               avatarBox: {
                 width: 35,
