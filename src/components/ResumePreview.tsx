c4cbd72b3e53ce0b782aa8c66178b3ca81f9de27
kartikgulati <kartikgulati19@gmail.com>
2025-04-02 20:26:56 -0400

    created the resume preview - in process

--- src/components/ResumePreview.tsx
+++ src/components/ResumePreview.tsx
@@ -1,19 +1,79 @@
+import useDimension from "@/hooks/useDimension";
 import { cn } from "@/lib/utils";
 
-import { ResumeValues } from "@/lib/validaton"; 
+import { ResumeValues } from "@/lib/validaton";
+import React, { useEffect, useState } from "react";
 
 interface ResumePreviewProps {
-    resumeData: ResumeValues;
-    className?: string;
+  resumeData: ResumeValues;
+  className?: string;
 }
 
-export default function ResumePreview({resumeData,className}:ResumePreviewProps) {
+export default function ResumePreview({
+  resumeData,
+  className,
+}: ResumePreviewProps) {
+    const containerRef = React.useRef<HTMLDivElement>(null);
 
-    return (
-        <div className={cn("bg-white text-black h-fit w-full aspect-[210/297]", className)}>
-            <h1>this text will change based on the container size</h1>
+    const { Width: width } = useDimension(containerRef)
+  return (
+    <div
+      className={cn(
+        "bg-white text-black h-fit w-full aspect-[210/297] ",
+        className
+      )}
+      ref={containerRef}
+    >
+        <div
+        className= {cn("space-y-6 p-6",!width && "invisible")}
+        style={{
+            zoom: (1/794)*width,
             
-        </div>
-    )
+        }}>
+            <PersonalInfoHeader resumeData={resumeData} />
+      </div>
+    </div>
+  );
+}
 
+interface ResumePreviewSectionProps {
+  resumeData: ResumeValues;}
+
+  function PersonalInfoHeader({resumeData}: ResumePreviewSectionProps) {
+    const {photo, firstName, lastName, email, phone, city, country, jobTitle}= resumeData;
+
+    const [photoSrc, setPhotoSrc] = useState(photo instanceof File ? "" : photo);
+
+    useEffect(() => {
+        const objectUrl = photo instanceof File ? URL.createObjectURL(photo) : "";
+        if (objectUrl) setPhotoSrc(objectUrl);
+        if (photo === null) setPhotoSrc("");
+        return () => URL.revokeObjectURL(objectUrl);
+      }, [photo]);
+
+      return <div className="flex items-center">
+        {
+            photoSrc && (
+                // eslint-disable-next-line @next/next/no-img-element
+                <img
+                src={photoSrc}
+                alt="Profile"
+                width={100}
+                height={100}
+                className="aspect-square rounded-full"
+                />
+            )
+        }
+
+        <div className="space-y-2.5">
+            <div className="space-y-1">
+                <p className="text-2xl font-bold">
+                    {firstName} {lastName}
+                </p>
+                <p className="font-medium">{jobTitle}</p>
+            </div>
+            <p className="text-xs text-grey-500">
+                {city && country ? `${city}, ` : ""} {country}</p>
+        </div>
+      </div>
 }
---
+++ src/hooks/useDimension.ts
@@ -0,0 +1,41 @@
+import { WindSong } from "next/font/google";
+import React, { use, useEffect, useState } from "react";
+import { set } from "zod";
+
+
+
+export default function useDimension(containerRef: React.RefObject<HTMLDivElement>){    
+    const [dimensions, setDemensions] = useState({Width: 0, Height: 0});
+
+    useEffect(() => {
+        const currentRef = containerRef.current;
+
+        function getDimensions() {
+            return {
+                Width: currentRef?.offsetWidth || 0,
+                Height: currentRef?.offsetHeight || 0
+            }
+        }
+
+        const resizeObserver = new ResizeObserver((entries) => {
+            const entry = entries[0];
+            if (entry) {
+                setDemensions(getDimensions());
+
+            }
+        })
+        if (currentRef) {
+            resizeObserver.observe(currentRef);
+            setDemensions(getDimensions());
+        }
+        return () => {
+            if (currentRef) {
+                resizeObserver.unobserve(currentRef);
+            }
+            resizeObserver.disconnect();
+        }
+
+
+    },[containerRef]);
+    return dimensions;
+}
