3a04a923b74859e2f6398fe03744e265ca9a41a5
kartikgulati <kartikgulati19@gmail.com>
2025-04-07 23:08:44 -0400

    Resume preview

--- src/app/(main)/editor/ResumePreviewSection.tsx
+++ src/app/(main)/editor/ResumePreviewSection.tsx
@@ -1,23 +1,29 @@
 import ResumePreview from "@/components/ResumePreview";
 import { ResumeValues } from "@/lib/validaton";
 import ColorPicker from "./ColorPicker";
+import BorderStyleButton from "./BorderStyleButton";
 
 interface ResumePreviewSectionProps {
     resumeData: ResumeValues;
     setResumeData: (data: ResumeValues) => void;
 }
 
-export default function ResumePreviewSection({resumeData, setResumeData}) {
+export default function ResumePreviewSection({resumeData, setResumeData}: ResumePreviewSectionProps) {
     return (
-        <div className="relative hidden w-1/2 md:flex">
-            <div className="absolute top-1 left-1 flex flex-col gap-2 flex-none lg:top-3 lg:left-3">
+        <div className="group relative hidden w-1/2 md:flex">
+            <div className="opacity-50 xl:opacity-100 transition-opacity group-hover:opacity-100 absolute top-1 left-1 flex flex-col gap-2 flex-none lg:top-3 lg:left-3">
                 <ColorPicker
-                color={resumeData.color} onChange={(color) => setResumeData({...resumeData, colorHex: color.hex})} />
+                color={resumeData.colorHex} 
+                onChange={(color) => setResumeData({...resumeData, colorHex: color.hex})} />
+
+                <BorderStyleButton
+                borderStyle={resumeData.borderStyle} 
+                onChange={(borderStyle) => setResumeData({...resumeData, borderStyle})} />
+                
             </div>
             <div className="flex w-full justify-center overflow-y-auto bg-secondary p-3">
                 <ResumePreview resumeData={resumeData} className="max-w-2xl shadow-md" />
             </div>
         </div>
-          
     )
 }
--- src/components/ResumePreview.tsx
+++ src/components/ResumePreview.tsx
@@ -8,6 +8,7 @@ import React, { useEffect, useState } from "react";
 import { formatDate } from "date-fns";
 import exp from "constants";
 import { Badge } from "@/components/ui/badge";
+import { BorderStyles } from "@/app/(main)/editor/BorderStyleButton";
 
 interface ResumePreviewProps {
   resumeData: ResumeValues;
@@ -70,6 +71,14 @@ interface ResumePreviewSectionProps {
                 width={100}
                 height={100}
                 className="aspect-square object-cover p-2"
+                style={{
+                  borderRadius: 
+                  borderStyle === BorderStyles.SQUARE 
+                  ? "0px" 
+                  : borderStyle === BorderStyles.CIRCLE
+                  ? "9999px"
+                  : "10%",
+                }}
                 />
             )
         }
@@ -83,7 +92,10 @@ interface ResumePreviewSectionProps {
                 }}> 
                     {firstName} {lastName}
                 </p>
-                <p className="font-medium">{jobTitle}</p>
+                <p className="font-medium" style={{
+                    color: colorHex
+                   
+                }}>{jobTitle}</p>
             </div>
             <p className="text-xs text-grey-500">
                 {city && country ? `${city}, ` : " "} 
@@ -97,14 +109,21 @@ interface ResumePreviewSectionProps {
 }
 
 function SummarySection({resumeData}: ResumePreviewSectionProps) {
-    const {summary} = resumeData;
+    const {summary,colorHex} = resumeData;
      if (!summary) return null;
 
      return(
       <>
-     <hr className="border-2"/>
+     <hr className="border-2" style={{
+                    borderColor: colorHex
+                   
+                }}/>
      <div className="space-y-3 break-inside-avoid">
-      <p className="text-lg font-semibold">Profesional profile</p>
+      <p className="text-lg font-semibold"
+      style={{
+        color: colorHex
+       
+    }}>Profesional profile</p>
       <div className="whitespace-pre-line text-sm">{summary}</div>
 
      </div>
@@ -114,15 +133,23 @@ function SummarySection({resumeData}: ResumePreviewSectionProps) {
 
 function WorkExperienceSection ({resumeData}: ResumePreviewSectionProps) {
   
-  const {workExperiences} = resumeData
+  const {workExperiences,colorHex} = resumeData
   if (!workExperiences?.length) return null;
    
 
    return(
     <>
-   <hr className="border-2"/>
+   <hr className="border-2"
+   style={{
+    borderColor: colorHex
+   
+}}/>
    <div className="space-y-3 break-inside-avoid">
-    <p className="text-lg font-semibold">Work Experience</p>
+    <p className="text-lg font-semibold"
+    style={{
+      color: colorHex
+     
+  }}>Work Experience</p>
     <div className="space-y-3">
       {workExperiences.map((experience, index) => (
         <div key={index} className="break-inside-avoid space-y-1">
@@ -151,7 +178,7 @@ function WorkExperienceSection ({resumeData}: ResumePreviewSectionProps) {
 
 
 function EducationSection({ resumeData }: ResumePreviewSectionProps) {
-  const { educations } = resumeData;
+  const { educations, colorHex } = resumeData;
     const educationsNotEmpty = educations?.filter(
     (edu) => Object.values(edu).filter(Boolean).length > 0
   )
@@ -159,9 +186,15 @@ function EducationSection({ resumeData }: ResumePreviewSectionProps) {
 
   return(
     <>
-   <hr className="border-2"/>
+   <hr className="border-2" style={{
+                    borderColor: colorHex
+                   
+                }}/>
    <div className="space-y-3 break-inside-avoid">
-    <p className="text-lg font-semibold">Education</p>
+    <p className="text-lg font-semibold" style={{
+                    color: colorHex
+                   
+                }}>Education</p>
     <div className="space-y-3">
       {educations.map((edu, index) => (
         <div key={index} className="break-inside-avoid space-y-1">
@@ -191,17 +224,26 @@ function EducationSection({ resumeData }: ResumePreviewSectionProps) {
 }
 
 function SkillsSection({ resumeData }: ResumePreviewSectionProps) {
-  const { skills } = resumeData;
+  const { skills, colorHex } = resumeData;
   if (!skills?.length) return null;
 
   return (
     <>
-    <hr className="border-2"/>
+    <hr className="border-2" style={{
+                    borderColor: colorHex
+                   
+                }}/>
     <div className="space-y-3 break-inside-avoid">
-      <p className="text-lg font-semibold">Skills</p>
+      <p className="text-lg font-semibold" style={{
+                    color: colorHex
+                   
+                }}>Skills</p>
       <div className="flex flex-wrap gap-2 break-inside-avoid">
         {skills.map((skill, index) => (
-          <Badge key={index} className="bg-black text-white rounded-md">
+          <Badge key={index} className="bg-black text-white rounded-md" style={{
+            backgroundColor: colorHex
+           
+        }}>
             {skill}
           </Badge>
           
