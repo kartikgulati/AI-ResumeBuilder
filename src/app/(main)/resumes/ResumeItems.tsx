b2a1cd84c78b9947e7a2b6e8f1cb0ac76c24fca0
kartikgulati <kartikgulati19@gmail.com>
2025-05-01 23:48:35 -0400

    preview resume on the home page.

--- src/app/(main)/resumes/ResumeItems.tsx
+++ src/app/(main)/resumes/ResumeItems.tsx
@@ -1,11 +1,19 @@
 "use client"
 
 import ResumePreview from "@/components/ResumePreview";
+import { Button } from "@/components/ui/button";
 import { ResumeServerData } from "@/lib/types"
 import { mapToResumeValues } from "@/lib/utils";
+import { DropdownMenu, DropdownMenuTrigger, DropdownMenuContent, DropdownMenuItem } from "@/components/ui/dropdown-menu";
 import { formatDate } from "date-fns";
 import Link from "next/link";
 import { format } from "path";
+import { useState, useTransition } from "react";
+import { MoreHorizontal, Copy, Trash, Edit } from "lucide-react";
+import { useToast } from "@/hooks/use-toast";
+import { deleteResume } from "./actions";
+import { Dialog ,DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle } from "@/components/ui/dialog";
+import LoadingButton from "@/components/LoadingButton";
 
 interface ResumeItemsProps {
     resume: ResumeServerData
@@ -13,7 +21,7 @@ interface ResumeItemsProps {
 
 export default function ResumeItems({resume}: ResumeItemsProps) {
     const wasUpdated = resume.updatedAt !== resume.createdAt;
-    return <div className=" group border rounded-lg border-transparent hover:border-border transition-colors bg-secondary p-3">
+    return <div className=" relative group border rounded-lg border-transparent hover:border-border transition-colors bg-secondary p-3">
         <div className="space-y-3">
             <Link href={`/editor?resumeId=${resume.id}`} className="inline-block w-full text center">
             <p className="font-semibold line-clamp-1">
@@ -37,5 +45,95 @@ export default function ResumeItems({resume}: ResumeItemsProps) {
                 className="overflow-hiddenshadow-sm transition-shadow group-hover: shadow-lg" />
             </Link>
         </div>
+
+        <MoreMenu resumeId={resume.id} />
     </div>
 } 
+
+interface MoreMenuProps {
+    resumeId: string;
+}
+
+function MoreMenu({resumeId}: MoreMenuProps) {
+    const [showDeleteConformation, setShowDeleteConfirmation] = useState(false);
+
+    return <>
+     <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                    <Button
+                        variant="ghost"
+                        size="icon"
+                        className="absolute right-0.5 top-0.5 opacity-0 group-hover:opacity-100 transition-opacity"
+                    >
+                        <MoreHorizontal className="h-4 w-4" />
+                    </Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent align="end" className="w-60">
+                    <DropdownMenuItem onClick={() => {}}>
+                        <Edit className="h-4 w-4 mr-2" />
+                        Edit
+                    </DropdownMenuItem>
+                    <DropdownMenuItem 
+                        onClick={() => setShowDeleteConfirmation(true)}
+                        className="text-destructive"
+                    >
+                        <Trash className="h-4 w-4 mr-2" />
+                        Delete
+                    </DropdownMenuItem>
+                </DropdownMenuContent>
+            </DropdownMenu>
+            <DeleteConfirmation resumeId={resumeId} open={showDeleteConformation} onOpenChange={setShowDeleteConfirmation} />
+    </>
+}
+
+interface DeleteConfirmationProps {
+    resumeId: string;
+    open: boolean;
+    onOpenChange: (open: boolean) => void
+}
+
+function DeleteConfirmation({resumeId, open, onOpenChange}: DeleteConfirmationProps) {
+    const {toast} = useToast();
+    const [isPending, startTransition] = useTransition();
+
+    async function handleDelete(){
+        startTransition(async () => {
+            try {
+                await deleteResume(resumeId);
+                onOpenChange(false);
+                window.location.reload();
+            } catch (error) {
+                console.error(error);
+                toast({
+                    variant: "destructive",
+                    description: "Something went wrong while deleting the resume",
+                })
+            } finally {
+                onOpenChange(false);
+                window.location.reload();
+            }
+        })
+    }
+
+
+    return <Dialog open={open} onOpenChange={onOpenChange}>
+        <DialogContent>
+            <DialogHeader>
+                <DialogTitle>Delete Resume?</DialogTitle>
+                <DialogDescription>
+                    Are you sure you want to delete this resume? This action cannot be undone.
+                </DialogDescription>
+            </DialogHeader>
+            <DialogFooter>
+                <LoadingButton
+                variant="destructive"
+                onClick={handleDelete}
+                loading={isPending}
+                >
+                    Delete
+                </LoadingButton>
+                <Button onClick={() => onOpenChange(false)} >Cancel</Button>
+            </DialogFooter>
+        </DialogContent>
+    </Dialog>
+}   
--- src/app/(main)/resumes/actions.ts
+++ src/app/(main)/resumes/actions.ts
@@ -1,3 +1,44 @@
 "use server"
 
-export default function
+import prisma from "@/lib/ prisma";
+import { auth } from "@clerk/nextjs/server"
+import { del } from "@vercel/blob";
+import { revalidatePath } from "next/cache";
+
+export async function deleteResume(id:string){
+    const { userId } = await auth();
+
+    if (!userId){
+        throw new Error("User not authenticated");
+    }
+
+    const resume = await prisma.resume.findUnique({
+        where: { id, userId }
+    })
+
+    if (!resume){   
+        throw new Error("Resume not found");
+    }
+
+    if (resume.photoUrl){
+        await del(resume.photoUrl);
+    }
+
+    await prisma.$transaction(async (tx) => {
+        // Delete related work experiences
+        await tx.workExperience.deleteMany({
+            where: { resumeID: id }
+        });
+
+        // Delete related educations
+        await tx.education.deleteMany({
+            where: { resumeID: id }
+        });
+
+        // Finally delete the resume
+        await tx.resume.delete({
+            where: { id }
+        });
+    });
+    revalidatePath("/resumes");
+}
