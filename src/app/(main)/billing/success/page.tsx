a64e22c70eb9f055f535e8bcfc185abcc83ebc7f
kartikgulati <kartikgulati19@gmail.com>
2025-05-09 11:29:44 -0400

    fixing the stripe check out

--- src/app/(main)/billing/success/page.tsx
+++ src/app/(main)/billing/success/page.tsx
@@ -1,3 +1,18 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
+
 export default function Page() {
-    return <main>Billing success page</main>;
+    return <main className="max-w-7xl mx-auto space-y-6 px-3 py-6 text-center">
+        <h1 className="text-3xl font-bold"> Billing successful</h1>
+        <p>
+            Successfully subscribed to the pro account, you can start creating resumes.
+        </p>
+        <Button
+        asChild
+        className=""
+        >
+        <Link href="/resumes">Go to Resumes</Link>
+        </Button>
+    </main>;
 }
