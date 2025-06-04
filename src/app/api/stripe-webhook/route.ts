ec5a7cfcc0dd15a9194fd44e06e0827676a04ea2
kartikgulati <kartikgulati19@gmail.com>
2025-06-04 00:07:58 -0400

    webhook errors

--- src/app/api/stripe-webhook/route.ts
+++ src/app/api/stripe-webhook/route.ts
@@ -1,80 +1,113 @@
 import { NextRequest } from "next/server";
-import stripe, { Stripe } from "stripe";
+import { Stripe } from "stripe";
 import { env } from "@/env";
 import { log } from "console";
 import { clerkClient } from "@clerk/nextjs/server";
+import stripe from "@/lib/stripe";
+import prisma from "@/lib/ prisma";
 
 export async function POST(req: NextRequest) {
+  try {
+    const payload = await req.text();
+    const signature = req.headers.get("stripe-signature");
 
-    try{
-
-        const payload  = await req.text();
-        const signature = req.headers.get("stripe-signature");
-
-        if (!signature){
-            return new Response ("Missing Signatures",{status: 400});
-        }
-
-        const event  = Stripe.webhooks.constructEvent(
-            payload,
-            signature,
-            env.STRIPE_WEBHOOK_SECRET
-        )
-
-        console.log(`Received event:${event.type}`,event.data.object);
-
-        switch(event.type){
-            case "checkout.session.completed":
-                await handleSessionCompleted(event.data.object)
-                break;
-            
-            case "customer.subscription.created":
-            case "customer.subscription.updated":
-                await handleSubscriptionCreatedOrUpdated(event.data.object.id);
-                break;
-
-            case "customer.subscription.deleted":
-                await handleSubscriptionDeleted(event.data.object.id);
-                break;
-
-                default:
-                    console.log(`uknown evet type: ${event.type}`);
-
-        }   
-
-        return new Response("Webhook Received Successfully", {status:200})
-
-    }catch(error){
-        console.error(error);
-        return new Response("Internal Server Error" ,{status:500})
+    if (!signature) {
+      return new Response("Missing Signatures", { status: 400 });
     }
+
+    const event = Stripe.webhooks.constructEvent(
+      payload,
+      signature,
+      env.STRIPE_WEBHOOK_SECRET
+    );
+
+    console.log(`Received event:${event.type}`, event.data.object);
+
+    switch (event.type) {
+      case "checkout.session.completed":
+        await handleSessionCompleted(event.data.object);
+        break;
+
+      case "customer.subscription.created":
+      case "customer.subscription.updated":
+        await handleSubscriptionCreatedOrUpdated(event.data.object.id);
+        break;
+
+      case "customer.subscription.deleted":
+        await handleSubscriptionDeleted(event.data.object as Stripe.Subscription);
+        break;
+
+      default:
+        console.log(`uknown evet type: ${event.type}`);
+    }
+
+    return new Response("Webhook Received Successfully", { status: 200 });
+  } catch (error) {
+    console.error(error);
+    return new Response("Internal Server Error", { status: 500 });
+  }
 }
 
-    async function handleSessionCompleted(session: Stripe.Checkout.Session){
+async function handleSessionCompleted(session: Stripe.Checkout.Session) {
+  const userId = sessionStorage.metadata?.userId;
 
-        const userId = sessionStorage.metadata?.userId;
+  if (!userId) {
+    console.error("No user found in the session.");
+  }
 
-        if(!userId){
-            console.error("No user found in the session.")
-        }
+  (await clerkClient()).users.updateUserMetadata(userId, {
+    privateMetadata: {
+      stripeCustomerId: session.customer as string,
+    },
+  });
+}
 
-        (await clerkClient()).users.updateUserMetadata(userId, {
-            privateMetadata:{
-                stripeCustomerId: session.customer as string,
-            }
-        })
-        
+async function handleSubscriptionCreatedOrUpdated(subscriptionId: string) {
+  const subscription = await stripe.subscriptions.retrieve(subscriptionId);
 
-    }
+  if (
+    subscription.status === "active" ||
+    subscription.status === "trialing" ||
+    subscription.status === "past_due"
+  ) {
+    await prisma.userSubscription.upsert({
+      where: {
+        userId: subscription.metadata.userId,
+      },
+      create: {
+        userId: subscription.metadata.userId,
+        stripeSubscriptionId: subscription.id,
+        stripeCustomerId: subscription.customer as string,
+        stripePriceId: subscription.items.data[0].price.id,
+        stripeCurrentPeriodEnd: new Date(
+          subscription.current_period_end * 1000
+        ),
+        stripeCancelAtPeriodEnd: subscription.cancel_at_period_end,
+      },
+      update:{ 
+        stripePriceId: subscription.items.data[0].price.id,
+        stripeCurrentPeriodEnd: new Date(
+          subscription.current_period_end * 1000
+        ),
+        stripeCancelAtPeriodEnd: subscription.cancel_at_period_end,
+      }
+    });
+  } else {
+    await prisma.userSubscription.deleteMany({
+      where: {
+        stripeCustomerId: subscription.customer as string,
+      },
+    });
+  }
+}
 
-async function handleSubscriptionCreatedOrUpdated(subscriptionId:string) {
-        console.log("Subscription created or updated");
-    }
-    
-async function handleSubscriptionDeleted(subscriptionId:string) {
-        console.log("Subscription deleted");
-    }
-
+async function handleSubscriptionDeleted(subscription: Stripe.Subscription) {
+  await prisma.userSubscription.deleteMany({
+      where: {
+        stripeCustomerId: subscription.customer as string,
+      },
+    });
+}
 
 // import { env } from "@/env";
 // import prisma from "@/lib/ prisma";
