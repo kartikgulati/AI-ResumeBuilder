a4ed2a010d17c6d75127d0146140f8476202a5a1
kartikgulati <kartikgulati19@gmail.com>
2025-08-29 09:24:10 -0400

    chnaged the params

--- src/app/(main)/editor/page.tsx
+++ src/app/(main)/editor/page.tsx
@@ -1,19 +1,54 @@
+// import { Metadata } from "next";
+// import ResumeEditor from "./ResumeEditor";
+// import prisma from "@/lib/prisma";
+// import { auth } from "@clerk/nextjs/server";
+// import { resumeDataInclude } from "@/lib/types";
+
+// // interface PageProps {
+// //   searchParams?: { resumeId?: string };
+// // }
+
+// export const metadata: Metadata = {
+//   title: "Design your dream resume",
+// };
+
+// export default async function Page({ searchParams }: { searchParams?: { resumeId?: string } }) {
+//   const { resumeId } = searchParams || {};
+
+//   const { userId } = await auth();
+
+//   if (!userId) {
+//     return null;
+//   }
+
+//   const resumeToEdit = resumeId
+//     ? await prisma.resume.findUnique({
+//         where: { id: resumeId, userId },
+//         include: resumeDataInclude,
+//       })
+//     : null;
+
+//   return <ResumeEditor resumeToEdit={resumeToEdit} />;
+// }
+
 import { Metadata } from "next";
 import ResumeEditor from "./ResumeEditor";
 import prisma from "@/lib/prisma";
 import { auth } from "@clerk/nextjs/server";
 import { resumeDataInclude } from "@/lib/types";
 
-// interface PageProps {
-//   searchParams?: { resumeId?: string };
-// }
-
 export const metadata: Metadata = {
   title: "Design your dream resume",
 };
 
-export default async function Page({ searchParams }: { searchParams?: { resumeId?: string } }) {
-  const { resumeId } = searchParams || {};
+interface PageProps {
+  searchParams?: {
+    resumeId?: string;
+  };
+}
+
+export default async function Page({ searchParams }: PageProps) {
+  const resumeId = searchParams?.resumeId;
 
   const { userId } = await auth();
 
